c0d9b7e58193913d1c7580746a1fd12174d101b9
kayhon <kayhon@gmail.com>
2018-03-22 14:49:10 -0700

    final_update_with_pics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 $( document ).ready(function(){
     // This Generates random number to guess
-   
+    var Random=Math.floor(Math.random()*102+19)
     
     // This will Display random number
-    
+    $('#orderToMatch').text(Random);
     
     //This will Generate random number for each Turtle
     var num1= Math.floor(Math.random()*12+1)
@@ -22,23 +22,76 @@ $( document ).ready(function(){
   
   //This will Reset game
   function reset(){
-       
+        Random=Math.floor(Math.random()*102+19);
+        console.log(Random)
+        $('#orderToMatch').text(Random);
+        num1= Math.floor(Math.random()*12+1);
+        num2= Math.floor(Math.random()*12+1);
+        num3= Math.floor(Math.random()*12+1);
+        num4= Math.floor(Math.random()*12+1);
+        playerTotal= 0;
+        $('#totalScore').text(playerTotal);
+        } 
   // This will Display wins
-  function (){
-  alert("");
-   
+  function woohoo(){
+  alert("Congrats its PIZZA TIME! You won!");
+    wins++; 
+    $('#wins').text(wins);
+    reset();
+  }
   // This will Display losses
-  function (){
-  alert ();
-   
+  function loser(){
+  alert ("Sorry! Bummer Dude no pizza time! You lose!");
+    losses++;
+    $('#losses').text(losses);
+    reset()
   }
   // Clicking on Turtles
     $('.red').on ('click', function(){
-      
+      playerTotal = playerTotal + num1;
+      console.log("New playerTotal= " + playerTotal);
+      $('#totalScore').text(playerTotal); 
+            //These are Win & lose conditions
+          if (playerTotal == Random){
+            woohoo();
+          }
+          else if ( playerTotal > Random){
+            loser();
+          }   
+    })  
     $('.blue').on ('click', function(){
-      
+      playerTotal = playerTotal + num2;
+      console.log("New playerTotal= " + playerTotal);
+      $('#totalScore').text(playerTotal); 
+          if (playerTotal == Random){
+            woohoo();
+          }
+          else if ( playerTotal > Random){
+            loser();
+          } 
+    })  
     $('.orange').on ('click', function(){
-      
+      playerTotal = playerTotal + num3;
+      console.log("New playerTotal= " + playerTotal);
+      $('#totalScore').text(playerTotal);
+  
+            if (playerTotal == Random){
+            woohoo();
+          }
+          else if ( playerTotal > Random){
+            loser();
+          } 
+    })  
     $('.purple').on ('click', function(){
-     
- 
+      playerTotal = playerTotal + num4;
+      console.log("New playerTotal= " + playerTotal);
+      $('#totalScore').text(playerTotal); 
+        
+            if (playerTotal == Random){
+            woohoo();
+          }
+          else if ( playerTotal > Random){
+            loser();
+          }
+    });   
+  }); 
