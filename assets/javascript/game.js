95ecb59db012cf9cb0df6dc222fbc11c61222142
kayhon <kayhon@gmail.com>
2018-03-22 13:57:28 -0700

    changes4update3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,23 @@
 // this can be the javacript file for the crystal game
-//the number generated @ the begining of each round by the machine between 12-120 - this is a function or a process that I want the computer run over and over. Write the function in javascript and call it in jQuery - although in terms of syntax the jQuery goes before the javascript
+//the number generated @ the begining of each round by the main between 12-120 - this is a function or a process that I want the computer run over and over. Write the function in javascript and call it in jQuery - although in terms of syntax the jQuery goes before the javascript
 (document).ready(function(){
-    var machineTotal = Math.floor(Math.random()*101 + 19);
-    console.log(machineTotal);
+    var mainTotal = Math.floor(Math.random()*101 + 19);
+    console.log(mainTotal);
     });
     
-    //appending the machine total # so I can use it in html through an id - appending is a jQuery thing
-    $('#targetScore').text(machineTotal);
-    console.log(machineTotal);
+    //appending the main total # so I can use it in html through an id - appending is a jQuery thing
+    $('#targetScore').text(mainTotal);
+    console.log(mainTotal);
     
     //so the sum of the selected/clicked button and the current totalScore - not sure about this pseudocode - i think that the variable should be set to the begining state of totalScore, so that I can write a function with this variable.  How do I best distinguish the difference between these things at this state of writing?
     //1.jQuery goes first
     
     var totalScore = 0;
     
-    //===when machineTotal === totalScore - same question here - how do I know that the variable needs to be defined as its starting state.  At this point, because I've been thinking about it so long, I know that I'm going to use this variable as a way to add and record (maybe thats it - the place where it gets recorded, variables are the place where changing #'s get stored??')
+    //===when mainTotal === totalScore - same question here - how do I know that the variable needs to be defined as its starting state.  At this point, because I've been thinking about it so long, I know that I'm going to use this variable as a way to add and record (maybe thats it - the place where it gets recorded, variables are the place where changing #'s get stored??')
     var wins = 0;
     
-    // ===when total Score is > machineTotal - same question here
+    // ===when total Score is > mainTotal - same question here
     var losses = 0;
     
     //jQuery needs the variables to be called, looks similar to appending code above
@@ -30,11 +30,11 @@
     //The buttons script included in start game function
     function start () {
         (document).ready(function(){
-        var machineTotal = Math.floor(Math.random()*101 + 19);
-        console.log(machineTotal);
+        var mainTotal = Math.floor(Math.random()*101 + 19);
+        console.log(mainTotal);
         //appending it to an id that Im defining so html can use it
-        $('#targetScore').text(machineTotal);
-        console.log(machineTotal);
+        $('#targetScore').text(mainTotal);
+        console.log(mainTotal);
         //this is the button that generates (and hides behind an image) random # between 1 - 12
         var crystalButton1 = Math.floor(Math.random()*11+1);
         //this is the button that generates (and hides behind an image) and random # between 1 - 12
@@ -67,35 +67,34 @@
     
     }
     
-    //this is getting the buttons to work.  append each variable with jQuery so html can use it. Give each button an onclick function so it will activate when the user clicks on the image, then define the function you want the machine to perform - which is adding the image # with the total score #.
+    //this is getting the buttons to work.  append each variable with jQuery so html can use it. Give each button an onclick function so it will activate when the user clicks on the image, then define the function you want the main to perform - which is adding the image # with the total score #.
     $('#buttonOne').click(function() {
         totalScore = totalScore + crystalButton1;
-        console.log();
+        console.log(totalScore + crystalButton1);
     });
               $('#finalTotal').text(totalScore); 
               //win operation
-            if (){
-                win();
+            if (totalScore == mainTotal){
+              success();
             }
             //lose operation
-            else if (totalScore > machineTotal){
+            else if (totalScore > mainTotal){
               fail();
-            
     }
             //repeat for 3 other buttons
     
     $('#buttonTwo').click(function() {
         totalScore = totalScore + crystalButton2;
-        console.log();
-              $('#finalTotal').text(totalScore); 
-              //win operation
-            if (){
-              win();
-            }
-            //lose operation
-            else if (();
-            }
-    
+        console.log(totalScore + crystalButton2);
+                $('#finalTotal').text(totalScore); 
+                 //win operation
+                 if (totalScore == mainTotal){
+                      success();
+                 }
+                //lose operation
+                else if (totalScore > mainTotal){
+                    fail();
+        }
     $('#buttonThree').click(function() {
         totalScore = totalScore + crystalButton3;
         console.log();
